1c2303996039bd7711bb633c9c7dee0b04d249a5
Aniruddha S J <aniruddhajaganatha@gmail.com>
2023-06-14 02:46:54 +0530

    tweet image and footer

--- types/index.ts
+++ types/index.ts
@@ -2,14 +2,14 @@ export type userType={
     id: string,
     username: string,
     name: string,
-    image?:string
+    image?:string|undefined
 }
 export type tweetType={
     id: string,
     user: userType,
     createdAt: string,
     content: string,
-    image?:string,
+    image?:string|undefined,
     numberOfComments?: number,
     numberOfRetweets?: number,
     numberOfLikes?: number,
