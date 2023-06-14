9542ed42b5e7b5097219702c43b8f4e810ed7d63
Aniruddha S J <aniruddhajaganatha@gmail.com>
2023-06-15 03:26:50 +0530

    working on drawer navigation

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,7 +1,19 @@
-import { withLayoutContext } from 'expo-router';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-const DrawerNavigation=createDrawerNavigator().Navigator
-const Drawer=withLayoutContext(DrawerNavigation)
+import { withLayoutContext } from "expo-router";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+const DrawerNavigation = createDrawerNavigator().Navigator;
+const Drawer = withLayoutContext(DrawerNavigation);
+export const unstable_settings = {
+  initialRouteName: "(tabs)",
+};
 export default () => {
-    return <Drawer/>
-}
+  return (
+    <Drawer>
+      <Drawer.Screen
+        name="(tabs)"
+        options={{ title: "Home", headerShown: false }}
+      />
+      <Drawer.Screen name="bookmarks" options={{ title: "Bookmarks" }} />
+      <Drawer.Screen name="twitter-blue" options={{ title: "Twitter Blue" }} />
+    </Drawer>
+  );
+};
